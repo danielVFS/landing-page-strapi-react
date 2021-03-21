f6b541719c177fd10173aa2938f448824189dc3a
danielVFS <danielv759153@gmail.com>
2021-03-21 19:52:40 -0300

    testing: Heading Component

--- frontend/src/templates/Home/Home.test.jsx
+++ frontend/src/templates/Home/Home.test.jsx
@@ -4,15 +4,8 @@ import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
 import { theme } from '../../styles/theme';
 
-test('render Home', () => {
-  const { debug } = renderTheme(<Home />);
-
-  const headingContainer = screen.getByRole('heading', { name: 'Hello World' })
-    .parentElement;
-
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
