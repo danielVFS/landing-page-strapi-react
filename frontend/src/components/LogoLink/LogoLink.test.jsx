096d2dba96c45232f74ccf37ceaee5c9a08693f3
danielVFS <danielv759153@gmail.com>
2021-03-22 16:19:15 -0300

    testing: LogoLink

--- frontend/src/components/LogoLink/LogoLink.test.jsx
+++ frontend/src/components/LogoLink/LogoLink.test.jsx
@@ -3,8 +3,35 @@ import { renderTheme } from '../../styles/render-theme';
 import { LogoLink } from '.';
 
 describe('<LogoLink />', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>Children</LogoLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Hello World" />);
+
+    expect(
+      screen.getByRole('heading', { name: 'Hello World' }),
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: 'Hello World' })).toHaveAttribute(
+      'href',
+      '#target',
+    );
+  });
+
+  it('should render logo img', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Hello World" srcImg="image.jpg" />,
+    );
+
+    expect(screen.getByAltText('Hello World')).toHaveAttribute(
+      'src',
+      'image.jpg',
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <LogoLink link="#target" text="Hello World" srcImg="image.jpg" />,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
