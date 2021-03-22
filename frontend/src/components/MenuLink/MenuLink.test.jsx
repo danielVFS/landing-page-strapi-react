8b9aca41d91e3c7e49d9e68b42feec6267f9c9db
danielVFS <danielv759153@gmail.com>
2021-03-22 09:49:30 -0300

    testing: MenuLinkComponent

--- frontend/src/components/MenuLink/MenuLink.test.jsx
+++ frontend/src/components/MenuLink/MenuLink.test.jsx
@@ -3,8 +3,71 @@ import { renderTheme } from '../../styles/render-theme';
 import { MenuLink } from '.';
 
 describe('<MenuLink />', () => {
-  it('should render', () => {
-    renderTheme(<MenuLink>Children</MenuLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="www.google.com.br/">Children</MenuLink>);
+
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should open a link in new tab', () => {
+    renderTheme(
+      <MenuLink link="www.google.com.br/" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(
+      <MenuLink link="www.google.com.br/" newTab={false}>
+        Children
+      </MenuLink>,
+    );
+
+    expect(screen.getByRole('link', { name: 'Children' }))
+      .toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        color: #0A1128;
+        position: relative;
+      }
+
+      .c0::after {
+        content: '';
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        width: 0;
+        height: 0.2rem;
+        background: #dc143c;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c0:hover::after {
+        left: 25%;
+        width: 50%;
+      }
+
+      <a
+        class="c0"
+        href="www.google.com.br/"
+        target="_self"
+      >
+        Children
+      </a>
+    `);
   });
 });
