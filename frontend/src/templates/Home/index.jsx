4e159f07079ec05f6e57e23553fedced41d8fc95
danielVFS <danielv759153@gmail.com>
2021-03-26 20:56:23 -0300

    frontend: fetch data

--- frontend/src/templates/Home/index.jsx
+++ frontend/src/templates/Home/index.jsx
@@ -1,9 +1,38 @@
-import * as Styled from './styles';
+import { useEffect, useState } from 'react';
 
+import * as Styled from './styles';
 import { BaseTemplate } from '../BaseTemplate';
 import { mockBase } from '../BaseTemplate/mock';
+import { mapData } from '../../api/map-data';
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch(
+        'http://localhost:1337/pages/?slug=landing-page',
+      );
+      const json = await data.json();
+      const pageData = mapData(json);
+
+      console.log(json);
+      console.log(pageData);
+
+      setData(pageData[0]);
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined) {
+    return <h1>Página não encontrada</h1>;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>;
+  }
+
   return <BaseTemplate {...mockBase} />;
 }
 
