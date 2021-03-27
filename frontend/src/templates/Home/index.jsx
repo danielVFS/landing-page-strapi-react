ddcc47dca587e5bf64bbdbec1811b0874e59d9a8
danielVFS <danielv759153@gmail.com>
2021-03-27 09:13:11 -0300

    Component 404: Page Not Found

--- frontend/src/templates/Home/index.jsx
+++ frontend/src/templates/Home/index.jsx
@@ -4,29 +4,31 @@ import * as Styled from './styles';
 import { BaseTemplate } from '../BaseTemplate';
 import { mockBase } from '../BaseTemplate/mock';
 import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFound';
 
 function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const load = async () => {
-      const data = await fetch(
-        'http://localhost:1337/pages/?slug=landing-page',
-      );
-      const json = await data.json();
-      const pageData = mapData(json);
+      try {
+        const data = await fetch(
+          'http://localhost:1337/pages/?slug=landing-pages',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
 
-      console.log(json);
-      console.log(pageData);
-
-      setData(pageData[0]);
+        setData(pageData[0]);
+      } catch (error) {
+        setData(undefined);
+      }
     };
 
     load();
   }, []);
 
   if (data === undefined) {
-    return <h1>Página não encontrada</h1>;
+    return <PageNotFound />;
   }
 
   if (data && !data.slug) {
---
+++ frontend/src/templates/PageNotFound/index.jsx
@@ -0,0 +1,10 @@
+import { GridContent } from '../../components/GridContent';
+
+export const PageNotFound = () => {
+  return (
+    <GridContent
+      title="Error 404"
+      html="<p>A página que você procura não foi encontrada.</p>"
+    />
+  );
+};
