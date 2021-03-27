f053f8979449725e0e5a28c6effc2c9ddc5d70d3
danielVFS <danielv759153@gmail.com>
2021-03-27 13:55:38 -0300

    load components on frontend

--- frontend/src/templates/Home/index.jsx
+++ frontend/src/templates/Home/index.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from 'react';
 
 import * as Styled from './styles';
+
 import { BaseTemplate } from '../BaseTemplate';
 import { mockBase } from '../BaseTemplate/mock';
+
 import { mapData } from '../../api/map-data';
+
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+
+import { GridTwoColumns } from '../../components/GridTwoColumns';
+import { GridContent } from '../../components/GridContent';
+import { GridImage } from '../../components/GridImage';
+import { GridText } from '../../components/GridText';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -36,7 +44,37 @@ function Home() {
     return <Loading />;
   }
 
-  return <BaseTemplate {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <BaseTemplate
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumns key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </BaseTemplate>
+  );
 }
 
 export default Home;
